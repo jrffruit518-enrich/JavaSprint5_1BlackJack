c3b0788aea68b7d3ff75acc65932f33c38dd9ae5
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-24 16:48:23 +0100

    feat: initial commit

---
+++ src/main/java/com/example/JavaSprint5_1BlackJack/JavaSprint51BlackJackApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaSprint5_1BlackJack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprint51BlackJackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprint51BlackJackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaSprint5_1BlackJack/JavaSprint51BlackJackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaSprint5_1BlackJack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprint51BlackJackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
