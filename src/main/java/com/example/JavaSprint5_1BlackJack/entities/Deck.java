36911d807539a50082d9e5e061c89ca7b8d32819
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-24 21:11:24 +0100

    feat: add deck

---
+++ src/main/java/com/example/JavaSprint5_1BlackJack/entities/Deck.java
@@ -0,0 +1,46 @@
+package com.example.JavaSprint5_1BlackJack.entities;
+
+import com.example.JavaSprint5_1BlackJack.enums.Rank;
+import com.example.JavaSprint5_1BlackJack.enums.Suit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Deck {
+
+    private  List<Card> cards = new ArrayList<>();
+
+    public List<Card> getDeck() {
+        return List.copyOf(cards);
+    }
+
+
+    public Card draw() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("The deck is empty! Cannot draw more cards.");
+        }
+        return cards.removeFirst();
+    }
+
+    public int remaining() {
+        return cards.size();
+    }
+
+    public static Deck createNewShuffledDeck() {
+        List<Card> cards = new ArrayList<>();
+        for (Suit s:Suit.values()) {
+            for (Rank r : Rank.values()) {
+               cards.add(new Card(s,r));
+            }
+        }
+        Collections.shuffle(cards);
+        return new Deck(cards);
+    }
+}
