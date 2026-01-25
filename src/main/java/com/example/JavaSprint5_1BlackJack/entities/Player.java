c07101c9dcd0abc2e49c9244410550799b0f784f
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-25 15:22:57 +0100

    fix: update player

--- src/main/java/com/example/JavaSprint5_1BlackJack/entities/Player.java
+++ src/main/java/com/example/JavaSprint5_1BlackJack/entities/Player.java
@@ -6,25 +6,38 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.Objects;
+
 @Table("players") // R2DBC 的表映射注解
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Player {
 
     @Id
     @Column("play_id")
-    private Long playId;
+    private Long playerId;
 
+    @Setter
+    @NonNull
     @Column("player_name") // 明确指定数据库字段名
     private String playerName;
 
     @Column("total_win")
-    private Integer totalWin;
+    private Integer totalWin = 0;
 
     @Column("total_games")
-    private Integer totalGames;
+    private Integer totalGames =0;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(playerId, player.playerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(playerId);
+    }
 }
