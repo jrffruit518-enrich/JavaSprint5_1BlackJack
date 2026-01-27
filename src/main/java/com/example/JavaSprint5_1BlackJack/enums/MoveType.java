fd102ebc21a7544d05a25935e866826d072feb1c
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-27 20:55:34 +0100

    fix: update MoveType

--- src/main/java/com/example/JavaSprint5_1BlackJack/enums/MoveType.java
+++ src/main/java/com/example/JavaSprint5_1BlackJack/enums/MoveType.java
@@ -1,6 +1,13 @@
 package com.example.JavaSprint5_1BlackJack.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum MoveType {
     HIT,   // Ask for another card
-    STAND  // Keep current hand and end player's turn
+    STAND ; // Keep current hand and end player's turn
+
+    @JsonCreator
+    public static MoveType from(String value) {
+        return MoveType.valueOf(value.toUpperCase());
+    }
 }
